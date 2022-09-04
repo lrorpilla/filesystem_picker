f882a0e5e9473a75fb8482249ac046b5b48474fc
lrorpilla <leorafaelorpilla@gmail.com>
2022-09-04 12:58:14 +1000

    Icons

--- package/lib/src/utils/helpers/file_icon_helper.dart
+++ package/lib/src/utils/helpers/file_icon_helper.dart
@@ -7,7 +7,7 @@ class FileIconHelper {
   static Icon getIcon(String filename, Color color) {
     var icon = Icons.description;
 
-    final _extension = filename.split('.').last;
+    final _extension = filename.split('.').last.toLowerCase();
     if (_extension == 'db' ||
         _extension == 'sqlite' ||
         _extension == 'sqlite3') {
@@ -17,7 +17,12 @@ class FileIconHelper {
         _extension == 'bmp' ||
         _extension == 'png') {
       icon = Icons.image;
+    } else if (videoExtensions.contains(_extension)) {
+      icon = Icons.movie;
+    } else if (subtitlesExtensions.contains(_extension)) {
+      icon = Icons.subtitles;
     }
+
     // default
     return Icon(
       icon,
@@ -25,4 +30,46 @@ class FileIconHelper {
       size: iconSize,
     );
   }
+
+  static List<String> subtitlesExtensions = [
+    'srt',
+    'ass',
+  ];
+
+  static List<String> videoExtensions = [
+    '3gp',
+    'aaf',
+    'asf',
+    'avchd',
+    'avi',
+    'drc',
+    'flv',
+    'm2v',
+    'm4p',
+    'm4v',
+    'mkv',
+    'mng',
+    'mov',
+    'mp2',
+    'mp4',
+    'mpe',
+    'mpeg',
+    'mpg',
+    'mpv',
+    'mxf',
+    'nsv',
+    'ogg',
+    'ogv',
+    'ogm',
+    'qt',
+    'rm',
+    'rmvb',
+    'roq',
+    'srt',
+    'svi',
+    'vob',
+    'webm',
+    'wmv',
+    'yuv'
+  ];
 }
